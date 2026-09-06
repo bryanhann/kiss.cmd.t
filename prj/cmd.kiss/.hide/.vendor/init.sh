baf917930a5ab40c9a97939c0957edd58d1d54a9
Bryan Hann <nobody@nowhere>
2026-09-05 22:55:22 -0230

    Move [kiss] to [lbin/tmp.kiss]

--- prj/cmd.kiss/.hide/.vendor/init.sh
+++
@@ -1,3 +0,0 @@
-v=$(dirname ${BASH_SOURCE[0]})
-
-source $v/bash_colors.sh
--- prj/cmd.kiss/.hide/config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
-cfg_here=$PWD
-cfg_real=$PWD/main
-cfg_name=$(basename $PWD)
-cfg_name=${name:4}
-cfg_link=${BCH0_BIN:-$HOME/.local/bin}/$cfg_name
-popd > /dev/null
-
